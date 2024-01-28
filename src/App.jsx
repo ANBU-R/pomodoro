4b21d5c8c28e1033b71e14039aa267445dc6da12
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2024-01-28 21:18:44 +0530

    Integrate settings modal to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useSelector } from "react-redux";
 import "./App.css";
+import SettingsModal from "./features/Settings/SettingsModal";
 import TimeBox from "./features/Timer/TimeBox";
-import Nav from "./nav";
+import Nav from "./Nav";
 
 function App() {
+  const { pomodoro, shortBreak, longBreak } = useSelector(
+    (store) => store.timer
+  );
+  const { settingsVisibility } = useSelector((store) => store.settings);
   return (
     <div className="h-screen">
       <Nav />
       <main className="w-full flex flex-col justify-center items-center  bg-indigo-400 h-[90vh]">
         <TimeBox />
+        {settingsVisibility && (
+          <SettingsModal
+            pomodoro={pomodoro}
+            short={shortBreak}
+            long={longBreak}
+          />
+        )}
       </main>
     </div>
   );
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,17 @@
+import { useDispatch } from "react-redux";
 import whiteLogo from "./assets/logo-white.svg";
 import settingsLogo from "./assets/settings.svg";
+import { showSettings } from "./features/Settings/settingsSlice";
 const Nav = () => {
+  const dispatch = useDispatch();
+
   return (
     <nav className="nav">
       <div className="nav-logo">
         <img src={whiteLogo} className="h-[50%]" alt="pomoFoc-logo" />
         <h1 className="text-white font-bold font-mono">PomoFoc</h1>
       </div>
-      <button className="nav-button">
+      <button className="nav-button" onClick={() => dispatch(showSettings())}>
         <img src={settingsLogo} className="h-[100%]" alt="settings" />
       </button>
     </nav>
